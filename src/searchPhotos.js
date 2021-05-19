a154f541b89b36afbf519f6b540f80f4c89116aa
Amulya Dussa <amulyadussa@Amulyas-MacBook-Air.local>
2021-05-18 21:49:22 -0700

    add im'feeling lucky

--- src/searchPhotos.js
+++ src/searchPhotos.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Unsplash, { toJson } from "unsplash-js";
+import Button from "@material-ui/core/Button";
 
 const unsplash = new Unsplash({
   accessKey: "kimVYymB467M3FaTfdY3OZOyfPt_h4e6Pou23hAc_mc",
@@ -7,6 +8,9 @@ const unsplash = new Unsplash({
 export default function SearchPhotos() {
   const [query, setQuery] = useState("");
   const [pics, setPics] = useState([]);
+
+  // create a variable to dynamically constuct and assing the lucky URL
+  const [luckyURL, setLuckyURL] = useState("https://bing.com");
 
   const searchPhotos = async (e) => {
     // prevents page load on each search
@@ -22,6 +26,12 @@ export default function SearchPhotos() {
 
         // we are assing response from the unsplash to setpic variable
         setPics(json.results);
+
+        //lets get first element in json.results
+        console.log(json.results[1].urls.full);
+
+        // this line sets the LuckyURL use the reach method which gets assigned to lucky URL whcih is in the hfref with in the </a>
+        setLuckyURL(json.results[1].urls.full);
       });
   };
 
@@ -39,9 +49,17 @@ export default function SearchPhotos() {
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
+        <label className="label" htmlFor="query"></label>
+        <label className="label" htmlFor="query"></label>
         <button type="submit" className="button">
           Search
         </button>
+        <label className="label" htmlFor="query"></label>
+        <label className="label" htmlFor="query"></label>
+        <a type="submit" className="button lucky" href={luckyURL}>
+          I'm Feeling Lucky
+        </a>
+        <label className="label" htmlFor="query"></label>
       </form>
       <div className="card-list">
         {pics.map((pic) => (
